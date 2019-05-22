96e4919a1a36ceb4e5b4d1ee52ae9e7f2566e7c3
小小马蒂尼 <2509086054@qq.com>
2019-05-22 14:25:44 +0800

    蝉知CMS7.7原版

---
+++ system/config/my.php
@@ -0,0 +1,10 @@
+<?php
+$config->installed    = true;	
+$config->debug        = false;	
+$config->requestType  = 'PATH_INFO2';	
+$config->db->host     = 'localhost';	
+$config->db->port     = '3306';	
+$config->db->name     = 'eps';	
+$config->db->user     = 'root';	
+$config->db->password = 'root3306';		
+$config->db->prefix   = 'eps_';	
