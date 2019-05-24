b3cde89cffec948a11433a0bdb56c58f4e53cd37
小小马蒂尼 <2509086054@qq.com>
2019-05-24 11:20:49 +0800

    修改index模板

--- system/template/default/index/index.html.php
+++ system/template/default/index/index.html.php
@@ -1,8 +1,8 @@
 {if(!defined("RUN_MODE"))} {!die()} {/if}
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'header')}
-<div id='focus' class='block-list'>
-  <div class='row focus-top blocks' data-grid='12' data-region='index_index-top'>{$control->block->printRegion($layouts, 'index_index', 'top', true)}</div>
-  <div class='row focus-middle blocks' data-grid='4' data-region='index_index-middle'>{$control->block->printRegion($layouts, 'index_index', 'middle', true)}</div>
-  <div class='row focus-bottom blocks' data-grid='6' data-region='index_index-bottom'>{$control->block->printRegion($layouts, 'index_index', 'bottom', true)}</div>
-</div>
+
+{$control->block->printRegion($layouts, 'index_index', 'top', true)}
+{$control->block->printRegion($layouts, 'index_index', 'middle', true)}
+{$control->block->printRegion($layouts, 'index_index', 'bottom', true)}
+
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'footer')}
