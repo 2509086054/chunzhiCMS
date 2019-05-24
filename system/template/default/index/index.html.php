d60abf3d590e1c1b33bc30a8807a83508a2fdc17
小小马蒂尼 <2509086054@qq.com>
2019-05-24 15:00:00 +0800

    index模板不使用grid

--- system/template/default/index/index.html.php
+++ system/template/default/index/index.html.php
@@ -1,8 +1,8 @@
 {if(!defined("RUN_MODE"))} {!die()} {/if}
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'header')}
 
-{$control->block->printRegion($layouts, 'index_index', 'top', true)}
-{$control->block->printRegion($layouts, 'index_index', 'middle', true)}
-{$control->block->printRegion($layouts, 'index_index', 'bottom', true)}
+{$control->block->printRegion($layouts, 'index_index', 'top')}
+{$control->block->printRegion($layouts, 'index_index', 'middle')}
+{$control->block->printRegion($layouts, 'index_index', 'bottom')}
 
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'footer')}
