dc8f689c5474fa2f7ebd9a87d5dd84ef64f3089f
2509086054 <2509086054@qq.com>
2019-05-30 02:22:21 +0800

    footer只加载js和流量统计，业务block迁移到DaleFooter中

--- system/template/default/common/footer.html.php
+++ system/template/default/common/footer.html.php
@@ -1,59 +1,16 @@
 {if(!defined("RUN_MODE"))} {!die()} {/if}
-{!$control->block->printRegion($layouts, 'all', 'bottom', true)}
-<footer class="classic">
-  <section class="content-section parallax-bg-3" data-stellar-background-ratio=".15">
-    <div class="foot-wrapper">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3 anim fadeInLeft">
-          </div><!-- .col-lg-3 -->
-
-          <div class="col-md-3 anim fadeInLeft">
-          </div><!-- .col-lg-3 -->
-
-          <div class="col-md-3">
-          </div><!-- .col-lg-3 -->
-
-          <div class="col-md-3 anim fadeInRight">
-          </div><!-- .col-lg-3 -->
-        </div><!-- .row -->
-
-        <div class="row">
-          <div class="col-lg-12">
-            <span class="copyright">
-              {$copyright=empty($config->site->copyright) ? '' : $config->site->copyright . '-'}
-              {$contact=json_decode($config->company->contact)}
-              {$company=(empty($contact->site) or $contact->site == $control->server->http_host) ? $config->company->name : html::a('http://' . $contact->site, $config->company->name, "target='_blank'")}
-              copyright&copy; &nbsp;&nbsp;{$copyright} {!echo date('Y')} {$company} &nbsp;&nbsp;
-            </span>
-            <span class="miitbeian">
-              {if(!empty($config->site->icpLink) and !empty($config->site->icpSN))}
-              {!html::a(strpos($config->site->icpLink, 'http://') !== false ? $config->site->icpLink : 'http://' . $config->site->icpLink, $config->site->icpSN, "target='_blank'")}
-              {/if}
-              {if(empty($config->site->icpLink) and !empty($config->site->icpSN))}
-              {$config->site->icpSN}
-              {/if}
-              {if(!empty($config->site->policeLink) and !empty($config->site->policeSN))}
-              {!html::a(strpos($config->site->policeLink, 'http://') !== false ? $config->site->policeLink : 'http://' . $config->site->policeLink, html::image($webRoot . 'theme/default/default/images/main/police.png'),"target='_blank'")}
-              {/if}
-            </span>
-          </div><!-- .col-lg-12 -->
-        </div><!-- .row -->
-      </div><!-- .container -->
-    </div><!-- .foot-wrapper -->
-  </section><!-- .content-section -->
-
-</footer>
+{* !$control->block->printRegion($layouts, 'all', 'bottom') *}
+{include $control->loadModel('ui')->getEffectViewFile('default', 'block', 'DaleFooter.lite')}
 
 {* All JavaScript Loading begin *}
-
 {if($config->debug)}
 <!-- All JavaScript -->
 {!js::import($themeRoot . 'common/js/jquery-1.11.0.min.js')}
 <!-- Easing core JavaScript -->
 {!js::import($themeRoot . 'common/js/jquery.easing.1.3.js')}
 <!-- Bootstrap core JavaScript -->
-{!js::import($themeRoot . 'common/js/bootstrap.min.js')}
+{!js::import($themeRoot . 'common/js/popper.min.js')}
+{!js::import($themeRoot . 'common/js/bootstrap.min.3.1.js')}
 <!-- Master slider core JavaScript -->
 {!js::import($themeRoot . 'common/js/masterslider.min.js')}
 <!-- Master slider staff core JavaScript -->
@@ -88,6 +45,8 @@
 {!js::import($themeRoot . 'common/js/lightbox.min.js')}
 <!-- Everything else -->
 {!js::import($themeRoot . 'common/js/custom.js')}
+<!-- chanzhi JavaScript-->
+{!js::import($jsRoot . 'chanzhi.js')}
 {else}
 {if($cdnRoot)}
 {!js::import($cdnRoot . '/js/chanzhi.all.js', $version = false)}
@@ -97,6 +56,7 @@
 {/if}
 
 {* All JavaScript Loading end *}
+{include TPL_ROOT . 'common/log.html.php'}
 </body>
 
 </html>
