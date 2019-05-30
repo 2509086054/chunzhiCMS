d628d297c47b4e05d01e848991d3fe48458cf722
2509086054 <2509086054@qq.com>
2019-05-30 13:56:04 +0800

    加入站点地图和运行信息

--- system/template/default/block/DaleFooter.lite.html.php
+++ system/template/default/block/DaleFooter.lite.html.php
@@ -5,7 +5,7 @@
  * @Author: Alisa
  * @Date: 2019-05-28 12:20:46
  * @LastEditors: Alisa
- * @LastEditTime: 2019-05-30 01:29:09
+ * @LastEditTime: 2019-05-30 04:00:24
  */
 *}
 <footer class="classic">
@@ -24,18 +24,19 @@
         </div><!-- .row -->
 
         <div class="row">
-          <div class="col-lg-12">
-            <!-- 运行信息 -->
-            {if($config->site->execInfo == 'show')} {$config->execPlaceholder} {/if}
-            <!-- 站点地图 -->
-            {!html::a(helper::createLink('sitemap', 'index'), '<i class=\'fa fa-sitemap\'></i> ' . $lang->sitemap->common, "class='text-linki'")}
+        <div class="col-xs-4  anim fadeInLeft ">
+        <!-- 站点地图 -->
+        {!html::a(helper::createLink('sitemap', 'index'), '<i class=\'fa fa-sitemap\'></i> ' . $lang->sitemap->common, "class='text-linki'")}
+        </div>
+          <div class="col-xs-6">
             <span class="copyright">
-              {$copyright=empty($config->site->copyright) ? '' : $config->site->copyright . '-'}
               {$contact=json_decode($config->company->contact)}
               {$company=(empty($contact->site) or $contact->site == $control->server->http_host) ? $config->company->name : html::a('http://' . $contact->site, $config->company->name, "target='_blank'")}
-              copyright&copy; &nbsp;&nbsp;{$copyright} {!echo date('Y')} {$company} &nbsp;&nbsp;
+              {$company}
             </span>
-            <span class="miitbeian">
+            <span class="icpbeian">
+              {$copyright=empty($config->site->copyright) ? '' : $config->site->copyright . '-'}
+              &copy; &nbsp;&nbsp;{$copyright} {!echo date('Y')} &nbsp;&nbsp;
               {if(!empty($config->site->icpLink) and !empty($config->site->icpSN))}
               {!html::a(strpos($config->site->icpLink, 'http://') !== false ? $config->site->icpLink : 'http://' . $config->site->icpLink, $config->site->icpSN, "target='_blank'")}
               {/if}
@@ -46,7 +47,11 @@
               {!html::a(strpos($config->site->policeLink, 'http://') !== false ? $config->site->policeLink : 'http://' . $config->site->policeLink, html::image($webRoot . 'theme/default/default/images/main/police.png'),"target='_blank'")}
               {/if}
             </span>
-          </div><!-- .col-lg-12 -->
+          </div><!-- .col-xs-6 -->
+          <div class="col-xs-2  anim fadeInRight ">
+            <!-- 运行信息 -->
+            {if($config->site->execInfo == 'show')} {$config->execPlaceholder} {/if}
+          </div><!-- .col-xs-2 -->
         </div><!-- .row -->
 
       </div><!-- .container -->
