271b4d92b4e01744732e1d52e891eb9d5303e72c
2509086054 <2509086054@qq.com>
2019-05-31 13:55:02 +0800

    加入网站二维码存储位置信息

--- system/module/site/config.php
+++ system/module/site/config.php
@@ -1,4 +1,6 @@
-<?php if(!defined("RUN_MODE")) die();?>
+<?php if (!defined("RUN_MODE")) {
+    die();
+}?>
 <?php
 $config->site->help['sina'] = 'http://www.chanzhi.org/book/chanzhieps/41.html';
 $config->site->help['qq']   = 'http://www.chanzhi.org/book/chanzhieps/57.html';
@@ -7,7 +9,9 @@ $config->site->editor = new stdclass();
 $config->site->editor->setbasic = array('id' => 'pauseTip', 'tools' => 'simple');
 $config->site->editor->setagreement = array('id' => 'agreementContent', 'tools' => 'simple');
 
-if(!isset($config->site->mobileTemplate)) $config->site->mobileTemplate = 'close';
+if (!isset($config->site->mobileTemplate)) {
+    $config->site->mobileTemplate = 'close';
+}
 
 $config->themeSetting = new stdclass();
 $config->themeSetting->primaryColor = '#D1270A' ;
@@ -19,3 +23,14 @@ $config->cdn->fileList[] = '/theme/default/default/chanzhi.all.admin.css';
 $config->cdn->fileList[] = '/js/chanzhi.all.admin.js';
 $config->cdn->fileList[] = '/theme/default/default/chanzhi.all.css';
 $config->cdn->fileList[] = '/js/chanzhi.all.js';
+/**
+ * @Description:
+ * 加入网站二维码存储位置信息
+ * 由 system\framework\helper.class.php
+ * getSiteQrcode() 函数调用
+ * $config->webRoot = / or /www/
+ * @Author: Alisa
+ * @Date: 2019-05-31 10:35:48
+ */
+$config->site->qrcodeFrontDir = $config->webRoot . 'data/wechat/site/'; // front dir
+$config->site->qrcodeFileName   = 'qrcodeSite.png';
