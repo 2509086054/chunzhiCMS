bc5634065929729683b94ce332991e62583bcce2
2509086054 <2509086054@qq.com>
2019-06-01 12:21:28 +0800

    微修改，删除$icon，没用的css<i>标签

--- system/template/default/block/contact.html.php
+++ system/template/default/block/contact.html.php
@@ -5,14 +5,14 @@
  * @Author: Alisa
  * @Date: 2019-05-25 20:37:25
  * @LastEditors: Alisa
- * @LastEditTime: 2019-05-30 15:49:01
+ * @LastEditTime: 2019-06-01 12:04:11
  */
 *}
 {$block->content = json_decode($block->content)}
 {$contact = $model->loadModel('company')->getContact()}
 <div class="col-md-4">
   <!-- contact 联系我们 -->
-  <h5><i class="fa fa-rss"></i>{!echo $icon . $block->title}</h5>
+  <h5><i class="fa fa-rss"></i>{!echo $block->title}</h5>
   <div class="contact-info">
     {if(!empty($block->content->moreText) and !empty($block->content->moreUrl))}
       <p>
