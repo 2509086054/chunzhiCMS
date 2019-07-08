ed3befd5742156780b1c2d5685c66c393eca7ede
2509086054 <2509086054@qq.com>
2019-07-08 12:45:42 +0800

    html::image 改为 lazyloadImage

--- system/template/default/block/DaleSlide.html.php
+++ system/template/default/block/DaleSlide.html.php
@@ -30,7 +30,7 @@
         {$itemClass = 0 === $index++ ? 'item active' : 'item'}
         {if($slide->backgroundType == 'image')}
           <div data-id='{!echo $slide->id}' class='{!echo $itemClass }'{!echo $url . ' ' . $target}>
-          {!print(html::image($slide->image,"alt='{{$slide->title}}' title='{{$slide->title}}'"))}
+          {!print(html::lazyloadImage($slide->image,"alt='{{$slide->title}}' title='{{$slide->title}}'"))}
         {else}
           <div data-id='{!echo $slide->id}' class='{!echo $itemClass }'{!echo $url . ' ' . $target} style='{!echo 'background-color: ' . $slide->backgroundColor . '; height: ' . $height . 'px'}'>
         {/if}
